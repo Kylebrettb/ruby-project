13d40d4617c9f6614d26061e51e4e98c15014738
Rachel Beckstead <rachelbeckstead@Rachels-Air.domain>
2020-01-17 16:26:47 -0700

    updated validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -11,28 +11,28 @@ class Reservation < ApplicationRecord
     (reservation.check_out - reservation.check_in).to_i
   end
 
-	attr_accessor :checkin_date,  :checkout_date,
-
- validates_presence_of :checkin_date
- validates_presence_of :checkout_date
 
 
- validate :future_checkin_date
- validate :future_checkout_date
+ validates_presence_of :check_in
+ validates_presence_of :check_out
 
 
- def future_checkin_date
-	 if checkin_date.present? && checkin_date.to_date < DateTime.now.to_date
-		 errors.add(:checkin_date, 'must be a valid current or future date')
+ validate :future_check_in
+ validate :future_check_out
+
+
+ def future_check_in
+	 if check_in.present? && check_in.to_date < DateTime.now.to_date
+		 errors.add(:check_in, 'must be a valid current or future date')
 	 end
  end
 
 
 
- def future_checkout_date
-	 if checkin_date.present? && checkout_date.present? &&
-			 checkout_date.to_date < checkin_date.to_date
-		 errors.add(:checkout_date, 'must be a valid date after your check in ' \
+ def future_check_out
+	 if check_in.present? && check_out.present? &&
+			 check_out.to_date < check_in.to_date
+		 errors.add(:check_out, 'must be a valid date after your check in ' \
 			 'date')
 	 end
  end
